6b2ce10303829ca689e7404aabba50d7851bf1b7
Tamas Frei <frei.tamas555@gmail.com>
2019-01-08 15:08:28 +0100

    spawning powerups randomly

--- src/com/codecool/snake/GameLoop.java
+++ src/com/codecool/snake/GameLoop.java
@@ -3,7 +3,9 @@ package com.codecool.snake;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Animatable;
 import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.powerups.SimplePowerUp;
 import com.codecool.snake.entities.snakes.Snake;
+import com.codecool.snake.entities.snakes.SnakeHead;
 
 import java.util.List;
 
@@ -37,15 +39,25 @@ public class GameLoop {
 
     private void checkCollisions() {
         List<GameEntity> gameObjs = Globals.getInstance().display.getObjectList();
+
         for (int idxToCheck = 0; idxToCheck < gameObjs.size(); ++idxToCheck) {
             GameEntity objToCheck = gameObjs.get(idxToCheck);
+
             if (objToCheck instanceof Interactable) {
+
                 for (int otherObjIdx = idxToCheck + 1; otherObjIdx < gameObjs.size(); ++otherObjIdx) {
                     GameEntity otherObj = gameObjs.get(otherObjIdx);
+
                     if (otherObj instanceof Interactable){
+
                         if(objToCheck.getBoundsInParent().intersects(otherObj.getBoundsInParent())){
                             ((Interactable) objToCheck).apply(otherObj);
                             ((Interactable) otherObj).apply(objToCheck);
+
+                            if (objToCheck instanceof SimplePowerUp && otherObj instanceof SnakeHead || objToCheck instanceof SnakeHead && otherObj instanceof SimplePowerUp) {
+                                new SimplePowerUp();
+
+                            }
                         }
                     }
                 }
