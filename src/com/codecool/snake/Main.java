2688fa428ac521ba7c0cff760b78d169bbc56745
Aron Gida <ifAronisGida@github.com>
2019-01-10 16:45:11 +0100

    fixed goldsnitch texture

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -3,6 +3,8 @@ package com.codecool.snake;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,7 +16,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         Game game = new Game();
-        Scene mainScene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
+
+        Scene mainScene = new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT, Color.ANTIQUEWHITE);
+
+
 
         primaryStage.setTitle("Snake Game");
         primaryStage.setScene(mainScene);
