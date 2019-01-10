776d0729d08dd85e19aff032ff11317426bd7ca4
Csilla Urban <csillaurban@gmail.com>
2019-01-10 13:00:37 +0100

    pop up score at the end properly

--- src/com/codecool/snake/entities/snakes/Snake.java
+++ src/com/codecool/snake/entities/snakes/Snake.java
@@ -74,8 +74,8 @@ public class Snake implements Animatable {
     private void checkForGameOverConditions() {
         if (head.isOutOfBounds() || health <= 0) {
             System.out.println("Game Over");
-            showGameOverAlert();
             Globals.getInstance().stopGame();
+            showGameOverAlert();
         }
     }
 
